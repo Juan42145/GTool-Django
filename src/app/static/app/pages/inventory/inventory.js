d082515593e69e6a7665ef43595081fdc6fa0df9
Juan42145 <juancamilo.arias@hotmail.com>
2024-09-18 12:29:31 -0400

    Add gem total and change structure of weekly drops

--- src/app/static/app/pages/inventory/inventory.js
+++ src/app/static/app/pages/inventory/inventory.js
@@ -17,11 +17,35 @@ function renderInventory(){
 		/*Title*/createTxt(Sec, 'div', 'section__title', mCategory);
 
 		const Table = create(Sec, 'div', {'class':'section__table'});
+		let drops
 		Object.entries(mItems).forEach(([mItem, mMaterials], indexItem) => {
-			const Row = create(Table, 'div', {'class':'row'})
-			Row.style = 'grid-row: '+(indexItem + 1);
+			let Row
+			if(mCategory == "WEEKLY_DROPS"){
+				if (indexItem % 3 == 0){
+					Row = create(Table, 'div', {'class':'row', 'id':'WD-R_'+mMaterials.data})
+					Row.style = 'grid-row: '+(indexItem/3 + 1);
+					drops = ''
+					let total = getTotals()["WEEKLY_BOSSES"][mMaterials.data]
+					if (total !== undefined)
+						/*Total*/createTxt(Row, 'div', 'row__total',
+							total, {'id':'WD-I_'+mMaterials.data})
+				}
+				else{
+					Row = document.getElementById('WD-R_'+mMaterials.data)
+				}
+				/*Name*/
+				drops += '<p>- '+mItem+'</p>'
+				if (indexItem % 3 == 2){
+					const Text = create(Row, 'div', {'class':'row__name'})
+					Text.innerHTML = mMaterials.data + drops
+				}
+			} else{
+				Row = create(Table, 'div', {'class':'row'})
+				Row.style = 'grid-row: '+(indexItem + 1);
+				/*Name*/ createTxt(Row, 'div', 'row__name', mItem)
+			}
 
-			/*Name*/createTxt(Row, 'div', 'row__name', mItem)
+
 			
 			let total = getTotals()[mCategory]?.[mItem]
 			if (total !== undefined)
@@ -39,8 +63,26 @@ function renderInventory(){
 					uSet(userInv, [mCategory,mItem,rank], +Input.value)
 					storeUserI(user, userInv)
 					processTotals(mCategory, mItem);
+					if(mCategory == "GEMS"){
+						document.getElementById('gemC').textContent = getGemTotals(mItems)
+					}
+					if(mCategory == "WEEKLY_DROPS"){
+						document.getElementById('WD-I_'+mMaterials.data).textContent = getTotals()["WEEKLY_BOSSES"][mMaterials.data]
+					}
 				}, false);
 			});
 		});
+		if(mCategory == "GEMS"){
+			const TRow = create(Table, 'div', {'class':'row row--total'})
+			/*Name*/createTxt(TRow, 'div', 'row__name', 'Total')
+			createTxt(TRow, 'div','row__total', getGemTotals(mItems), {'id':'gemC'})
+		}
+
 	});
+}
+
+function getGemTotals(mItems){
+	return Object.keys(mItems).reduce((acc, mItem) => {
+		return acc + getTotals()["GEMS"][mItem]
+	}, 0)
 }
