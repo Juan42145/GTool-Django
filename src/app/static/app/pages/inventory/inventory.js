48be1ae00d3f0f2e142c243003ba7fcfa572c9dc
Juan42145 <juancamilo.arias@hotmail.com>
2025-02-04 12:38:58 -0500

    Refactor and style inventory

--- src/app/static/app/pages/inventory/inventory.js
+++ src/app/static/app/pages/inventory/inventory.js
@@ -12,52 +12,60 @@ function pageLoad(){
 function renderInventory(){
 	Object.entries(DBM).forEach(([mCategory, mItems]) => {
 		const Sec = document.getElementById(mCategory);
-		if (!Sec) return
+		if(!Sec) return
 
-		/*Title*/createTxt(Sec, 'div', 'section__title', mCategory);
+		const Btn = createTxt(Sec, 'div', 'section__btn accordion', mCategory.replace('_',' '));
 
-		const Table = create(Sec, 'div', {'class':'section__table'});
+		const Cont = createDiv(Sec, 'section__content hide');
+		if(['BOSSES','LOCAL_SPECIALTIES'].includes(mCategory))
+			Cont.classList.add('section__content--s')
+		makeAccordion(Btn, Cont)
 		let drops
 		Object.entries(mItems).forEach(([mItem, mMaterials], indexItem) => {
 			let Row
-			if(mCategory == "WEEKLY_DROPS"){
+			if(mCategory !== "WEEKLY_DROPS"){
+				Row = createDiv(Cont, 'item')
+				/*Name*/ createTxt(Row, 'div', 'item__name', mItem)
+			} else {
 				if (indexItem % 3 == 0){
-					Row = create(Table, 'div', {'class':'row', 'id':'WD-R_'+mMaterials.data})
-					Row.style = 'grid-row: '+(indexItem/3 + 1);
-					drops = ''
-					let total = getTotals()["WEEKLY_BOSSES"][mMaterials.data]
-					if (total !== undefined)
-						/*Total*/createTxt(Row, 'div', 'row__total',
-							total, {'id':'WD-I_'+mMaterials.data})
+					Row = createDiv(Cont, 'item', {'id':'WD-R_'+mMaterials.data})
+					drops = createDiv(Row, 'item__name--drops')
 				}
-				else{
-					Row = document.getElementById('WD-R_'+mMaterials.data)
-				}
+				else Row = document.getElementById('WD-R_'+mMaterials.data)
 				/*Name*/
-				drops += '<p>- '+mItem+'</p>'
+				createTxt(drops, 'p', '', '- '+mItem)
 				if (indexItem % 3 == 2){
-					const Text = create(Row, 'div', {'class':'row__name'})
-					Text.innerHTML = mMaterials.data + drops
+					const Text = createTxt(Row, 'div', 'item__name', mMaterials.data)
+					Text.appendChild(drops)
 				}
-			} else{
-				Row = create(Table, 'div', {'class':'row'})
-				Row.style = 'grid-row: '+(indexItem + 1);
-				/*Name*/ createTxt(Row, 'div', 'row__name', mItem)
 			}
 
+			let ItemCont
+			if (mCategory !== "WEEKLY_DROPS"){
+				ItemCont = createDiv(Row, 'item__cont')
+			} else {
+				if (indexItem % 3 == 0){
+					ItemCont = createDiv(Row, 'item__cont',{'id':'IC_'+mMaterials.data})
+					let total = getTotals()["WEEKLY_BOSSES"][mMaterials.data]
+					if (total !== undefined)
+						createTxt(ItemCont, 'div', 'item__total', total,
+							{'id':'WD-I_'+mMaterials.data})
+				}
+				else ItemCont = document.getElementById('IC_'+mMaterials.data)
+			}
 
-			
 			let total = getTotals()[mCategory]?.[mItem]
 			if (total !== undefined)
-				/*Total*/createTxt(Row, 'div', 'row__total',
+				/*Total*/createTxt(ItemCont, 'div', 'item__total',
 					Math.floor(total).toLocaleString('en-us'), {'id':'I_'+mItem})
+			
 			Object.keys(mMaterials).reverse().forEach((rank) => {
 				if (isNaN(rank)) return
-				const Card = create(Row, 'div', {'class':'row__card r_'+rank})
-				/*Img*/createImg(Card, 'row__card--img', getImage(mCategory, mItem, rank))
+				const Card = createDiv(ItemCont, 'item__card r_'+rank)
+				/*Img*/createImg(Card, 'item__card--img', getImage(mCategory, mItem, rank))
 				
 				let value = userInv[mCategory]?.[mItem]?.[rank] ?? 0
-				const Input = createNumInput(Card, {'data-column':rank}, value)
+				const Input = createNumInput(Card, {'class':'item__input','data-column':rank}, value)
 				Input.addEventListener('blur',() => {
 					Input.value = +Input.value;
 					uSet(userInv, [mCategory,mItem,rank], +Input.value)
@@ -73,12 +81,19 @@ function renderInventory(){
 			});
 		});
 		if(mCategory == "GEMS"){
-			const TRow = create(Table, 'div', {'class':'row row--total'})
-			/*Name*/createTxt(TRow, 'div', 'row__name', 'Total')
-			createTxt(TRow, 'div','row__total', getGemTotals(mItems), {'id':'gemC'})
+			const TRow = createDiv(Cont, 'item item--total')
+			/*Name*/createTxt(TRow, 'div', 'item__name', 'Total')
+			createTxt(TRow, 'div','item__total', getGemTotals(mItems), {'id':'gemC'})
 		}
 
 	});
+}
+
+function makeAccordion(accordion, panel){
+	accordion.addEventListener('click',() => {
+		accordion.classList.toggle('active')
+		panel.classList.toggle('hide')
+	})
 }
 
 function getGemTotals(mItems){
