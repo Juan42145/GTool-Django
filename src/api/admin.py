4e1932cb33fd5fc0a6ec9c3f019b7ab0e646d2af
Juan42145 <juancamilo.arias@hotmail.com>
2025-02-21 12:59:37 -0500

    Add admin custom functionality

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from .admin_site import *
 from .admin_class import *
 from .models import *
 
@@ -11,26 +12,26 @@ admin.site.register(User,UserAdmin)
 
 # Register your models here.
 register_app('app', 1)
-register(Character, 'app')
-register(Weapon, 'app')
+register(Character, 'app', admin_class=CharacterAdmin) 
+register(Weapon, 'app', admin_class=WeaponAdmin)
 
 register_app('dynamic', 2)
-register(LocalSpecialty, 'dynamic', 1)
-register(Boss, 'dynamic', 2)
-register(Enemy, 'dynamic', 3)
-register(WeeklyBoss, 'dynamic', 4)
-register(WeeklyDrop, 'dynamic', 5)
+register(LocalSpecialty, 'dynamic', 1, LSAdmin)
+register(Boss, 'dynamic', 2, SimpleAdmin)
+register(Enemy, 'dynamic', 3, EnemyAdmin)
+register(WeeklyBoss, 'dynamic', 4, SimpleAdmin)
+register(WeeklyDrop, 'dynamic', 5, WDAdmin)
 
 register_app('regional', 3)
-register(Region, 'regional', 1)
-register(Book, 'regional', 2)
-register(Trophy, 'regional', 3)
+register(Region, 'regional', 1, SimpleAdmin)
+register(Book, 'regional', 2, BookAdmin)
+register(Trophy, 'regional', 3, TrophyAdmin)
 
 register_app('static', 4)
-register(Element, 'static', 1)
-register(Stat, 'static', 2)
+register(Element, 'static', 1, ElementAdmin)
+register(Stat, 'static', 2, StatAdmin)
 register(Model, 'static', 3)
-register(WeaponType, 'static', 4)
+register(WeaponType, 'static', 4, SimpleAdmin)
 register(Resource, 'static', 5)
 
 register_app('image', 5)
--- src/api/admin_class.py
+++ src/api/admin_class.py
@@ -1,10 +1,6 @@
-from collections import defaultdict
-from types import SimpleNamespace
 from django.contrib import admin
 from django.shortcuts import redirect
-
-# Variable to determine ordering midpoint
-order_default = 10
+from .models import *
 
 # Custom model admin
 class SingletonAdmin(admin.ModelAdmin):
@@ -14,157 +10,72 @@ class SingletonAdmin(admin.ModelAdmin):
 
 class BaseAdmin(admin.ModelAdmin):
     search_fields = ["name"]
-    list_filter = ["name"]
+    actions = None
+    
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "region":
+            kwargs["queryset"] = Region.objects.order_by('-id')
+        elif db_field.name == "weekly_boss":
+            kwargs["queryset"] = WeeklyBoss.objects.order_by('-id')
+
+        elif db_field.name == "boss":
+            kwargs["queryset"] = Boss.objects.order_by('-id')
+        elif db_field.name == "local_specialty":
+            kwargs["queryset"] = LocalSpecialty.objects.order_by('-id')
+        elif db_field.name == "common" or db_field.name == "elite":
+            kwargs["queryset"] = Enemy.objects.order_by('-id')
+        elif db_field.name == "book":
+            kwargs["queryset"] = Book.objects.order_by('-id')
+        elif db_field.name == "weekly_drop":
+            kwargs["queryset"] = WeeklyDrop.objects.order_by('-id')
+        
+        elif db_field.name == "trophy":
+            kwargs["queryset"] = Trophy.objects.order_by('-id')
+
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+#Simple Models
+class SimpleAdmin(BaseAdmin):
+    list_display = ["name", "code"]
+    list_editable = ["code"]
+
+class ElementAdmin(BaseAdmin):
+    list_display = ["name", "id",  "inv_order", "gem"]
+    list_editable = ["inv_order"]
+
+class StatAdmin(BaseAdmin):
+    list_display = ["name", "is_weapon_stat"]
+
+class LSAdmin(BaseAdmin):
+    list_display = ["name", "region", "inv_order"]
+    list_editable = ["region", "inv_order"]
+    list_filter = ['region']
+
+class WDAdmin(BaseAdmin):
+    list_display = ["name", "weekly_boss"]
+    list_editable = ["weekly_boss"]
+
+#Regular Models
+class EnemyAdmin(BaseAdmin):
+    list_display = ["name", "is_elite", "low", "mid", "high"]
+    list_editable = ["is_elite", "low", "mid", "high"]
+
+class BookAdmin(BaseAdmin):
+    list_display = ["name", "id", "region"]
+
+class TrophyAdmin(BaseAdmin):
+    list_display = ["name", "id", "region", "code", "insert_code", "n2", "n3", "n4", "n5"]
+    list_editable = ["code", "insert_code", "n2", "n3", "n4", "n5"]
+
+class CharacterAdmin(BaseAdmin):
+    list_display = ["name", "id", "version", "rarity", "model", "region", "stat"]
+    list_editable = ["version"]
+    list_filter = ["version"]
+
+class WeaponAdmin(BaseAdmin):
+    list_display = ["name", "id", "version", "is_wish_only", "rarity", "weapon_type", "stat"]
+    list_editable = ["version"]
+    list_filter = ["version"]
 
 class ImageAdmin(BaseAdmin):
     list_display = ["name", "url", "format"]
-
-# Custom functions
-def register_app(label, order):
-    # Register app/module order
-    admin.site.ordering[label]['order'] = order if order>0 else order_default-order
-
-def register(cls, module, order=0, admin_class=BaseAdmin):
-    # Register model with module attribute and order
-    setattr(cls, 'module', module)
-    admin.site.ordering[module]['model'][cls._meta.verbose_name_plural] = order
-    admin.site.register(cls, admin_class)
-
-#Imports to replicate _build_app_dict
-from django.utils.text import capfirst
-from django.urls import NoReverseMatch, reverse
-from django.apps import apps
-
-# Custom admin
-class MyAdminSite(admin.AdminSite):
-    site_header = 'Administration'
-    site_title = ' '
-    ordering = defaultdict(lambda: {'order': order_default, 'model': defaultdict(lambda: float('inf'))})
-
-    #CUSTOM FUNCTIONS
-    def get_app(self, model, app_label):
-        # Create/Get app data
-        is_custom = hasattr(model, 'module')
-        label = model.module if is_custom else app_label
-        return SimpleNamespace(
-            label = label,
-            name = label.title() if is_custom else apps.get_app_config(app_label).verbose_name,
-            query = f'?sub={label}' if is_custom else '',
-            order = getattr(model,'','def')
-        )
-
-    def create_app_dict(self, param, app):
-        # Create app dictionary injecting custom values
-        app_label, has_module_perms, model_dict = param
-        return {
-            "name": app.name,
-            "app_label": app.label,
-            "app_url": reverse(
-                "admin:app_list",
-                kwargs={"app_label": app_label},
-                current_app=self.name,
-            ) + app.query,
-            "has_module_perms": has_module_perms,
-            "models": [model_dict],
-        }
-
-    # OVERWRITE FUNCTIONS
-    def _build_app_dict(self, request, label=None):
-        # Overwrite to allow app_dict insertion
-        """
-        Build the app dictionary. The optional `label` parameter filters models
-        of a specific app.
-        """
-        app_dict = {}
-
-        if label:
-            models = {
-                m: m_a
-                for m, m_a in self._registry.items()
-                if m._meta.app_label == label
-            }
-        else:
-            models = self._registry
-
-
-        for model, model_admin in models.items():
-            app_label = model._meta.app_label
-
-            has_module_perms = model_admin.has_module_permission(request)
-            if not has_module_perms:
-                continue
-
-            perms = model_admin.get_model_perms(request)
-
-            # Check whether user has any perm for this module.
-            # If so, add the module to the model_list.
-            if True not in perms.values():
-                continue
-
-            info = (app_label, model._meta.model_name)
-            model_dict = {
-                "model": model,
-                "name": capfirst(model._meta.verbose_name_plural),
-                "object_name": model._meta.object_name,
-                "perms": perms,
-                "admin_url": None,
-                "add_url": None,
-            }
-            if perms.get("change") or perms.get("view"):
-                model_dict["view_only"] = not perms.get("change")
-                try:
-                    model_dict["admin_url"] = reverse(
-                        "admin:%s_%s_changelist" % info, current_app=self.name
-                    )
-                except NoReverseMatch:
-                    pass
-            if perms.get("add"):
-                try:
-                    model_dict["add_url"] = reverse(
-                        "admin:%s_%s_add" % info, current_app=self.name
-                    )
-                except NoReverseMatch:
-                    pass
-
-            # CUSTOM Add app labels for admin dashboard (Source: https://github.com/django/django/blob/main/django/contrib/admin/sites.py)
-            app = self.get_app(model, app_label)
-            if app.label in app_dict:
-                app_dict[app.label]["models"].append(model_dict)
-            else:
-                param = (app_label, has_module_perms, model_dict)
-                app_dict[app.label] = self.create_app_dict(param, app)
-
-        return app_dict
-
-    def get_app_list(self, request, app_label=None):
-        # Overwrite to allow custom sorting
-        """
-        Return a sorted list of all the installed apps that have been
-        registered in this site.
-        """
-        app_dict = self._build_app_dict(request, app_label)
-
-        # Sort the apps alphabetically. CUSTOM: Added ordering
-        app_list = sorted(app_dict.values(), key=lambda x:
-                          (self.ordering[x["name"].lower()]['order'], x["name"].lower()))
-
-        # Sort the models alphabetically within each app.
-        for app in app_list:
-            order_dict = self.ordering[app['app_label']]['model']
-            app["models"].sort(key=lambda x:(order_dict[x["name"].lower()], x["name"].lower()))
-
-        return app_list
-
-    # VIEW FUNCTIONS
-    def app_index(self, request, app_label, extra_context = None):
-        # Include query string in view context
-        response = super().app_index(request, app_label, extra_context)
-        extra_context = extra_context or {}
-
-        is_custom = 'sub' in request.GET
-        extra_context['sub_app'] = request.GET['sub'] if is_custom else app_label
-        title = request.GET['sub'].title() if is_custom else response.context_data['title']
-        extra_context['title'] = title + ' Administration'if is_custom else title.replace('administration','Administration')
-        return super().app_index(request, app_label, extra_context)
-
-admin.site = MyAdminSite()
