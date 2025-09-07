606f718b7911c19641322d2a5102a9495a9ac24b
Juan42145 <juancamilo.arias@hotmail.com>
2025-09-07 12:19:37 -0400

    Update build_files.sh

--- src/build_files.sh
+++ src/build_files.sh
@@ -1,5 +1,5 @@
 # build_files.sh
-python3.9 - m pip install -r requirements.txt
+python - m pip install -r requirements.txt
 python3.9 manage.py collectstatic
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate
