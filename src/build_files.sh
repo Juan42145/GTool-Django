be0f3a7abc01f4db9d8dff0d604c2138e2d5f925
Juan42145 <juancamilo.arias@hotmail.com>
2025-09-07 12:47:48 -0400

    Update build_files.sh

--- src/build_files.sh
+++ src/build_files.sh
@@ -4,7 +4,6 @@ python3.9 -m venv venv
 source venv/bin/activate
 # install all deps in the venv
 pip install -r requirements.txt
-python3.9 - m pip install -r requirements.txt
 python3.9 manage.py collectstatic
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate
