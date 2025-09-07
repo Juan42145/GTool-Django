1564d0d22fe5d640e4b9cdba2cb56a5b746618c3
Juan42145 <juancamilo.arias@hotmail.com>
2025-09-07 15:25:17 -0400

    Refactor build_files.sh

--- src/build_files.sh
+++ src/build_files.sh
@@ -1,8 +1,6 @@
 # build_files.sh
-
 python3.9 -m venv venv
 source venv/bin/activate
-# install all deps in the venv
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
 python3.9 manage.py makemigrations
